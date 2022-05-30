b76105ef0eaa7652cde1b3841b6a0381195fc215
Melissa Schmohl <101364208+mschmohl@users.noreply.github.com>
2022-05-29 19:49:21 -0500

    Added JavaScript to animate balloons

--- js/script.js
+++ js/script.js
@@ -2,6 +2,12 @@ let colors = ['yellow', 'red', 'blue', 'violet', 'green'];
 let windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
 let body = document.body;
+let scores = document.querySelectorAll('.score');
+let num = 0;
+let total = 10;
+let currentBalloon = 0;
+let gameOver = false;
+let totalShadow = document.querySelector('.total-shadow');
 
 function createBalloon() {
     let div = document.createElement("div");
@@ -10,6 +16,8 @@ function createBalloon() {
 
     rand = Math.floor(Math.random() * (windowWidth - 100));
     div.style.left = rand + 'px';
+    div.dataset.number = currentBalloon;
+    currentBalloon++;
 
     body.appendChild(div);
     animateBalloon(div);
@@ -20,10 +28,9 @@ function createBalloon() {
      let interval = setInterval(frame, 10);
 
      function frame() {
-         console.log(pos);
-         if(pos >= (windowHeight + 200)) {
+         if(pos >= (windowHeight + 200) && (document.querySelector('[data-number="'+elem.dataset.number+'"]') !== null)) {
             clearInterval(interval);
-            deleteBalloon(elem);
+            gameOver = true;
          } else {
              pos++;
              elem.style.top = windowHeight - pos + 'px';
@@ -33,4 +40,36 @@ function createBalloon() {
 
  function deleteBalloon(elem) {
     elem.remove();
+    num++;
+    updateScore();
  }
+
+ function updateScore() {
+     for(let i = 0; i < scores.length; i++) {
+         scores[i].textContent = num;
+     }
+ }
+
+ function startGame() {
+     let loop = setInterval (function() {
+        if(!gameOver && num !== total) {
+            createBalloon();  
+        } else if(num !== total) {
+            clearInterval(loop);
+            totalShadow.style.display = 'flex';
+            totalShadow.querySelector('.lose').style.display = 'block';
+        } else {
+            clearInterval(loop);
+            totalShadow.style.display = 'flex';
+            totalShadow.querySelector('.win').style.display = 'block';
+        }
+     }, 800);
+ }
+
+ document.addEventListener('click', function(event) {
+    if(event.target.classList.contains('balloon')) {
+        deleteBalloon(event.target);
+    }
+ })
+
+ startGame()
